9158d257155f4ca7b565fd650b9158b8a7b6b176
DESKTOP-4TAOHB8\hunter <hunter10@naver.com>
2017-07-06 00:22:18 +0900

    네트웍 직전까지 작업완료

---
+++ Assets/02.Scripts/FireCannon.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCannon : MonoBehaviour {
+
+    public GameObject cannon = null;
+    public Transform firepos;
+
+    private AudioClip fireSfx = null;
+    private AudioSource sfx = null;
+
+    private void Awake()
+    {
+        cannon = (GameObject)Resources.Load("Cannon");
+
+        fireSfx = Resources.Load<AudioClip>("CannonFire");
+
+        sfx = GetComponent<AudioSource>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            fire();
+        }
+	}
+
+    void fire()
+    {
+        sfx.PlayOneShot(fireSfx, 1.0f);
+        Instantiate(cannon, firepos.position, firepos.rotation);
+    }
+}
